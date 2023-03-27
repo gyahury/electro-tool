2b0e3cfd1c0d911e64fa71c6dab7491d8eaa5620
gyahury <rudgns1243@naver.com>
2023-03-27 22:08:30 +0900

    close function added

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,3 +1,13 @@
+<script>export default {
+
+  methods: {
+      Close() {
+          window.close();
+      }
+      }
+  }
+</script>
+
 <template>
       <v-layout>
         <v-app-bar
@@ -9,8 +19,7 @@
             <v-btn icon="mdi-vuejs"></v-btn>
           </template>
           <template v-slot:append>
-            <v-btn icon="mdi-minus" size="small" style="color: rgb(100, 100, 100);"></v-btn>
-            <v-btn icon="mdi-close" size="small" style="color: rgb(100, 100, 100);"></v-btn>
+            <v-btn icon="mdi-close" @click="Close" size="small" style="color: rgb(100, 100, 100);"></v-btn>
           </template>
         </v-app-bar>
         <v-main>
@@ -19,7 +28,10 @@
   </template>
 
 <style>
-.v-toolbar__content {
-    -webkit-app-region: drag;
-}
+  .v-toolbar__content {
+      -webkit-app-region: drag;
+  }
+  .v-toolbar__append {
+      -webkit-app-region: no-drag;
+  }
 </style>
