cdce908d287a04262e2fa745cb9bab8e3fbab642
gyahury <rudgns1243@naver.com>
2023-04-07 22:23:47 +0900

    update : add menu and resize main height

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,20 +3,23 @@
         <v-layout>
             <v-navigation-drawer floating permanent elevation="3" color="rgb(255, 255, 255)">
                 <v-list nav>
+                    <v-list-item prepend-icon="mdi-home" title="HOME" value="home"></v-list-item>
                     <v-list-item prepend-icon="mdi-swap-horizontal" title="File Names To XLSX" value="filenamelist"
                         @click="toFileNameList"></v-list-item>
                     <v-list-item prepend-icon="mdi-swap-horizontal" title="XLSX To CSV" value="tocsv"
                         @click="toXlsxToCsv"></v-list-item>
                     <v-list-item prepend-icon="mdi-swap-horizontal" title="CSV To XLSX" value="toxlsx"
                         @click="toCsvToXlsx"></v-list-item>
-                    <!--<v-list-item prepend-icon="mdi-swap-horizontal" title="To JSON" value="tojson"></v-list-item>
-                    <v-list-item prepend-icon="mdi-swap-horizontal" title="이미지 해상도 출력" value="imageresol"></v-list-item>
+                    <v-list-item prepend-icon="mdi-swap-horizontal" title="ImgSize To XLSX" value="imgsizelist"
+                        @click="toImgSizeList">
+                    </v-list-item>
+                    <!--<v-list-item prepend-icon="mdi-swap-horizontal" title="이미지 해상도 출력" value="imageresol"></v-list-item>
                     <v-list-item prepend-icon="mdi-swap-horizontal" title="이미지 해상도 변경" value="chgimgresol"></v-list-item>
                     <v-list-item prepend-icon="mdi-swap-horizontal" title="폴더 내 파일 분리" value="sepfile"></v-list-item>
                     <v-list-item prepend-icon="mdi-swap-horizontal" title="폴더/파일명 일괄 변경" value="chafilename"></v-list-item> -->
                 </v-list>
             </v-navigation-drawer>
-            <v-main style="height: 550px;">
+            <v-main style="height: 480px;">
                 <v-sheet :height="250" :width="500" class="pa-12">
                     <router-view />
                 </v-sheet>
@@ -50,6 +53,12 @@ export default {
         },
         toCsvToXlsx() {
             this.$router.push("/CsvToXlsx")
+        },
+        toCsvToXlsx() {
+            this.$router.push("/CsvToXlsx")
+        },
+        toImgSizeList() {
+            this.$router.push("/ImgSizeList")
         }
     }
 }
